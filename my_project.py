b31a8a689109e3b5b8dec74b1ffdc3bb2ae08448
alek1984 <154059778+alek1984@users.noreply.github.com>
2025-04-01 18:16:33 +0300

    Add files via upload

---
+++ my_project.py
@@ -0,0 +1,14 @@
+[tool.poetry]
+name = "personal-assistant"
+version = "0.1.0"
+description = "My personal assistant project"
+authors = ["Your Name <your.email@example.com>"]
+readme = "README.md"
+
+[tool.poetry.dependencies]
+python = "^3.9"
+requests = "^2.26.0"
+
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"
